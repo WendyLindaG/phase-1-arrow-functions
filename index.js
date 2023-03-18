4b81321ec870c613f7993fe4728fc937b692c224
Wendy Linda Gakii <wendylinda2017@gmail.com>
2023-03-19 00:53:13 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+let w = 100;
+let r = 2000;
+let x = 8;
+let y = 9;
+let z = 7;
+const divide = function (a, b) {
+  return 2000 / 100;
+};
+
+const square = x => x * x;
+
+const add = (c, d) => {
+  return c + d;
+};
